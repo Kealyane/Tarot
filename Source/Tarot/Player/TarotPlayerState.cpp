00c5a987df92f940a0f0d59e5c0c7bbc827c5b1c
Laurene <laurene.broquet@gmail.com>
2024-10-19 18:45:49 +0200

    Create TarotPlayerState

---
+++ Source/Tarot/Player/TarotPlayerState.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TarotPlayerState.h"
+#include "Net/UnrealNetwork.h"
+
+ATarotPlayerState::ATarotPlayerState()
+{
+	ScoreLine0 = 0;
+	ScoreLine1 = 0;
+	ScoreLine2 = 0;
+	TotalScore = 0;
+}
+
+void ATarotPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ATarotPlayerState, ScoreLine0);
+	DOREPLIFETIME(ATarotPlayerState, ScoreLine1);
+	DOREPLIFETIME(ATarotPlayerState, ScoreLine2);
+	DOREPLIFETIME(ATarotPlayerState, TotalScore);
+	
+}
+
+void ATarotPlayerState::ComputeTotalScore()
+{
+	TotalScore = ScoreLine0 + ScoreLine1 + ScoreLine2;
+}
+
+
+void ATarotPlayerState::OnRep_ScoreLine0()
+{
+	OnScoreUpdated.Broadcast(PlayerPosition,0,ScoreLine0);
+}
+
+void ATarotPlayerState::OnRep_ScoreLine1()
+{
+	OnScoreUpdated.Broadcast(PlayerPosition,1,ScoreLine1);
+}
+
+void ATarotPlayerState::OnRep_ScoreLine2()
+{
+	OnScoreUpdated.Broadcast(PlayerPosition,2,ScoreLine2);
+}
+
+void ATarotPlayerState::OnRep_TotalScore()
+{
+	OnScoreUpdated.Broadcast(PlayerPosition,3,TotalScore);
+}
+
+void ATarotPlayerState::UpdatePocket_Implementation(EPosition Position, bool bHasArcane)
+{
+	OnPocketUpdate.Broadcast(Position, bHasArcane);
+}
