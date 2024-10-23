33760f7882bd70389653c729d02d50ec9292271b
Laurene <laurene.broquet@gmail.com>
2024-10-23 17:53:00 +0200

    LobbyGameMode init player position

--- Source/Tarot/GameModes/LobbyGameMode.cpp
+++ Source/Tarot/GameModes/LobbyGameMode.cpp
@@ -3,13 +3,30 @@
 
 #include "LobbyGameMode.h"
 #include "GameFramework/GameStateBase.h"
+#include "Tarot/Player/TarotPlayerController.h"
+#include "Tarot/Player/TarotPlayerState.h"
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
-
+	
+	if (ATarotPlayerController* TPC = CastChecked<ATarotPlayerController>(NewPlayer))
+	{
+		if (ATarotPlayerState* PlayerState = TPC->GetTarotPlayerState())
+		{
+			if (NumberOfPlayers == 1)
+			{
+				PlayerState->PlayerPosition = EPosition::LEFT;
+			}
+			else if (NumberOfPlayers == 2)
+			{
+				PlayerState->PlayerPosition = EPosition::RIGHT;
+			}
+		}
+	}
+	
 	if (NumberOfPlayers == 2)
 	{
 		if (UWorld* World = GetWorld())
